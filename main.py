a790e9e171cba294038272b15b55f7957e18426a
John Walker <jewalker7842@gmail.com>
2023-03-25 09:00:13 -0500

    fixed argument bug

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "-c, --computername",
+        "--computername",
         help="The computername of the client you wish to connect to.",
     )
     parser.add_argument(
-        "-u, --username", help="Username used to connect to the computername."
+        "--username", help="Username used to connect to the computername."
     )
-    parser.add_argument("-p, --password", help="Password used with the username")
+    parser.add_argument("--password", help="Password used with the username")
     args = parser.parse_args()
 
     computername = args.computername
